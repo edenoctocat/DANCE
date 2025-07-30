98af95a4e405a23f7bba714c8eacede13f7a40c1
edenoctocat <eden@edenwilliamreinfurt.com>
2025-07-30 12:53:14 -0400

    add play/pause on click

--- main.js
+++ main.js
@@ -1,44 +1,43 @@
 /** in this file, chooses random videos, loads, and plays */
 
 const videoElements = document.querySelectorAll(".my-video"); 
-/** const videoList = ['video1.mp4', 'video2.mp4', 'video3.mp4', 'video4.mp4', 'video5.mp4']; */
 const videoList = ['video1', 'video2', 'video3', 'video4', 'video5'];
-let videoIndex = 0;
 let remainingVids = videoList.slice()
 
-function chooseNext() {
-    if (remainingVids.length != 0) {
-        newIndex = Math.floor(Math.random() * remainingVids.length);
-        let currentVideo = document.getElementById(this.id);
-        let nextVideo = document.getElementById(remainingVids[newIndex]);
-        currentVideo.style.visibility = "hidden";
-        nextVideo.style.visibility = "visible";
-        nextVideo.play(); // doesn't like .play() without interaction in most browsers 
-        videoIndex = newIndex;
-        remainingVids.splice(videoIndex, 1);
-        console.log(remainingVids);
-    }
-    else {
+function playNext() {
+    let currentVideo = document.getElementById(this.id);
+    if (remainingVids.length == 0) {
         remainingVids = videoList.slice();
-        chooseNext();
     }
+    newVideo();
+    currentVideo.style.visibility = "hidden";
     console.log(remainingVids);
 }
 
-function startVideo() {
+function newVideo() {
     newIndex = Math.floor(Math.random() * remainingVids.length);
     console.log(newIndex);
     let nextVideo = document.getElementById(remainingVids[newIndex]);
     nextVideo.style.visibility = "visible";
-    nextVideo.addEventListener("click", function() {
-        nextVideo.play();
-    });
-    remainingVids.splice(videoIndex, 1);
+    nextVideo.play();
+    remainingVids.splice(newIndex, 1);
 }
 
-window.onload = startVideo();
+function playPause() {
+    let currentVideo = document.getElementById(this.id);
+    if ((currentVideo.currentTime <= 0 || currentVideo.paused || currentVideo.ended) && currentVideo.readyState > 2) {
+        console.log("playing");
+        currentVideo.play();
+    }
+    else {
+        console.log("pausing");
+        currentVideo.pause();
+    }
+}
+
+window.onload = newVideo();
 
 videoElements.forEach(element => {
-    element.addEventListener('ended', chooseNext);
-    /** element.addEventListener('onclick', playPause); */
+    element.addEventListener('ended', playNext);
+    element.addEventListener('click', playPause);
 });
