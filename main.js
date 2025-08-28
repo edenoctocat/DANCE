a66c30fdbaec78f5193d8e530f18fe0bedc0da52
edenoctocat <eden@edenwilliamreinfurt.com>
2025-08-28 13:17:07 -0400

    change space bar play/pause

--- main.js
+++ main.js
@@ -13,8 +13,7 @@ const state = {
 };
 
 
-// use api from server.js to get video list and init
-
+// initialize by getting video list from api and showing loading screen until loaded
 async function init() {
     const loading = document.getElementById("loading");
 
@@ -25,6 +24,9 @@ async function init() {
         const res = await fetch('/videolist');
         const videos = await res.json();
         console.log('video files from server:', videos);
+
+        if (!videos || videos.length === 0) return;
+
         state.videoList = videos;
         state.remainingVids = videos.slice(); // remainingVids set as copy of all videos
 
@@ -41,78 +43,76 @@ async function init() {
     }
 }
 
+// update index.html with videos from file
 function renderVideos(state) {
-    // update index.html with videos from file
-
     state.videoList.forEach(video => {
         const videoElement = document.createElement('video');
         videoElement.setAttribute('id', video); // id is same as file name
-        console.log('id:', video.slice(0, -4));
         videoElement.setAttribute('class', 'my-video');
         videoElement.setAttribute('src', videoDirectoryPath + video);
         videoElement.setAttribute('autoplay', '');
         videoElement.setAttribute('tabindex', '0');
-        // videoElement.setAttribute('type', 'video/mp4');
-        videoContainer.appendChild(videoElement);
+        videoContainer.appendChild(videoElement); // add each video player to video container
     }); 
 
 }
 
+// add event listeners to videos
 function setupVideoInteractions(state) {
-    // functions for video interaction 
-
-    function playNext(event) {
-        let currentVideo = event.target;
-        if (state.remainingVids.length == 0) {
-            state.remainingVids = state.videoList.slice();
-            state.remainingVids.splice(state.currentIndex, 1); // avoid repeating videos
-        }
-        newVideo();
-        currentVideo.style.visibility = "hidden";
-        currentVideo.currentTime = 0; // rewind video
-        console.log("remaining videos:", state.remainingVids);
-    }
-
-    function newVideo() {
-        let newIndex = Math.floor(Math.random() * state.remainingVids.length);
-        console.log("next video:", state.remainingVids[newIndex]);
-        let nextVideo = document.getElementById(state.remainingVids[newIndex]);
-        nextVideo.style.visibility = "visible";
-        nextVideo.play();
-        // update state
-        state.currentVideoId = state.remainingVids[newIndex];
-        state.remainingVids.splice(newIndex, 1);
-        console.log("state:", state);
-    }
-
-    function playPause(event) {
-        // let currentVideo = event.target;
-        let currentVideo = document.getElementById(state.currentVideoId);
-        if ((currentVideo.currentTime <= 0 || currentVideo.paused || currentVideo.ended) && currentVideo.readyState > 2) {
-            console.log("playing");
-            pausedText.classList.add('hidden');
-            currentVideo.play();
-        }
-        else {
-            console.log("pausing");
-            pausedText.classList.remove('hidden');
-            currentVideo.pause();
-        }
-    }
-
-    // adding event listeners to videos
-
     const videoElements = document.querySelectorAll(".my-video"); 
 
     videoElements.forEach(element => {
         element.addEventListener('ended', playNext);
         element.addEventListener('click', playPause);
-        element.addEventListener('keydown', playPause);
+        element.addEventListener('keydown', e => {
+                if (e.key === ' ' || e.key === 'Enter') playPause();
+            });
     }); 
 
     // load window with new video
     window.onload = newVideo();
 
+}
+
+// functions for video interaction 
+
+function playNext(event) {
+    let currentVideo = event.target;
+    if (state.remainingVids.length == 0) {
+        state.remainingVids = state.videoList.slice();
+        state.remainingVids.splice(state.currentIndex, 1); // avoid repeating videos
+    }
+    newVideo();
+    currentVideo.style.visibility = "hidden";
+    currentVideo.currentTime = 0; // rewind video
+    console.log("remaining videos:", state.remainingVids);
+}
+
+function newVideo() {
+    let newIndex = Math.floor(Math.random() * state.remainingVids.length);
+    console.log("next video:", state.remainingVids[newIndex]);
+    let nextVideo = document.getElementById(state.remainingVids[newIndex]);
+    nextVideo.style.visibility = "visible";
+    nextVideo.play();
+    // update state
+    state.currentVideoId = state.remainingVids[newIndex];
+    state.remainingVids.splice(newIndex, 1);
+    console.log("state:", state);
+}
+
+function playPause(event) {
+    // let currentVideo = event.target;
+    let currentVideo = document.getElementById(state.currentVideoId);
+    if ((currentVideo.currentTime <= 0 || currentVideo.paused || currentVideo.ended) && currentVideo.readyState > 2) {
+        console.log("playing");
+        pausedText.classList.add('hidden');
+        currentVideo.play();
+    }
+    else {
+        console.log("pausing");
+        pausedText.classList.remove('hidden');
+        currentVideo.pause();
+    }
 }
 
 init();
