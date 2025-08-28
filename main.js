05658fcb293562f34d92a955d30c8cc71875276a
edenoctocat <eden@edenwilliamreinfurt.com>
2025-08-28 16:28:37 -0400

    switch to using two alternating video players

--- main.js
+++ main.js
@@ -3,17 +3,19 @@
 const pausedText = document.getElementById('pausedText');
 const videoContainer = document.getElementById('my-container');
 const videoDirectoryPath = 'videos/';
+let videoElements = [];
 
 // group video state object
 const state = {
   videoList: [],      // all videos
   remainingVids: [],  // videos left to play
-  currentVideoId: null,  // the one playing right now
-  playing: false        // keep track of playing
+  nextFile: null,    // filename of video playing up next
+  currentIndex: null,  // index of video player in use
+  playing: false      // keep track of playing
 };
 
 
-// initialize by getting video list from api and showing loading screen until loaded
+// initialize by getting video list from api, choosing 2, and showing loading screen
 async function init() {
     const loading = document.getElementById("loading");
 
@@ -21,6 +23,7 @@ async function init() {
         loading.style.display = "block";
         videoContainer.style.display = "none";
 
+        // fetch all video file names from server
         const res = await fetch('/videolist');
         const videos = await res.json();
         console.log('video files from server:', videos);
@@ -30,12 +33,32 @@ async function init() {
         state.videoList = videos;
         state.remainingVids = videos.slice(); // remainingVids set as copy of all videos
 
-        renderVideos(state);
-        setupVideoInteractions(state);
+        videoElements = [
+            document.getElementById('video1'),
+            document.getElementById('video2')
+        ];
+
+        // add video player interaction
+        videoElements.forEach(element => {
+            element.addEventListener('ended', playNext);
+            element.addEventListener('click', playPause);
+            element.addEventListener('keydown', e => {
+                    if (e.key === ' ' || e.key === 'Enter') playPause();
+                });
+        }); 
+
+        // initialize video player
+        state.currentIndex = 1;
+        state.nextFile = newVideo();
+        let video1 = document.getElementById('video1');
+        video1.src = `videos/${state.nextFile}`;
 
         // hide loading once videos are ready
         loading.style.display = "none";
         videoContainer.style.display = "block";
+
+        // load window with new video
+        window.onload = playNext();
 
     } catch (err) {
         loading.textContent = "Error loading videos";
@@ -43,66 +66,49 @@ async function init() {
     }
 }
 
-// update index.html with videos from file
-function renderVideos(state) {
-    state.videoList.forEach(video => {
-        const videoElement = document.createElement('video');
-        videoElement.setAttribute('id', video); // id is same as file name
-        videoElement.setAttribute('class', 'my-video');
-        videoElement.setAttribute('src', videoDirectoryPath + video);
-        videoElement.setAttribute('autoplay', '');
-        videoElement.setAttribute('tabindex', '0');
-        videoContainer.appendChild(videoElement); // add each video player to video container
-    }); 
-
-}
-
-// add event listeners to videos
-function setupVideoInteractions(state) {
-    const videoElements = document.querySelectorAll(".my-video"); 
-
-    videoElements.forEach(element => {
-        element.addEventListener('ended', playNext);
-        element.addEventListener('click', playPause);
-        element.addEventListener('keydown', e => {
-                if (e.key === ' ' || e.key === 'Enter') playPause();
-            });
-    }); 
-
-    // load window with new video
-    window.onload = newVideo();
-
-}
-
 // functions for video interaction 
 
 function playNext(event) {
-    let currentVideo = event.target;
-    if (state.remainingVids.length == 0) {
-        state.remainingVids = state.videoList.slice();
-        state.remainingVids.splice(state.currentIndex, 1); // avoid repeating videos
-    }
-    newVideo();
+    let currentVideo = videoElements[state.currentIndex];
+    let nextIndex = 1 - state.currentIndex;
+    let nextVideo = videoElements[nextIndex];
+    state.currentIndex = nextIndex; // update index
+
+    // play next video and hide current video
+    console.log("playing", state.nextFile, "on video player", state.currentIndex);
+    nextVideo.style.visibility = "visible";
+    nextVideo.play().catch(err => console.warn('Play error:', err));
     currentVideo.style.visibility = "hidden";
-    currentVideo.currentTime = 0; // rewind video
+
+    if (state.remainingVids.length == 0) {
+        console.log("end of video list")
+        state.remainingVids = state.videoList.slice(); // reload all videos if done
+        const indexOfFile = state.remainingVids.indexOf(state.nextFile);
+        if (indexOfFile > -1) { 
+            state.remainingVids.splice(indexOfFile, 1); // avoid repeating videos directly
+        }
+    }
+    
     console.log("remaining videos:", state.remainingVids);
+
+    // load video that will play after next (to help speed)
+    state.nextFile = newVideo();
+    let videoAfter = currentVideo;
+    videoAfter.src = `videos/${state.nextFile}`; // will play after nextVideo
+    videoAfter.pause(); // make sure it is paused at beginning
+    videoAfter.currentTime = 0; // rewind video
+
+    console.log("playing video after:", state.nextFile);
 }
 
 function newVideo() {
+    // function to choose random video from remaining videos
     let newIndex = Math.floor(Math.random() * state.remainingVids.length);
-    console.log("next video:", state.remainingVids[newIndex]);
-    let nextVideo = document.getElementById(state.remainingVids[newIndex]);
-    nextVideo.style.visibility = "visible";
-    nextVideo.play();
-    // update state
-    state.currentVideoId = state.remainingVids[newIndex];
-    state.remainingVids.splice(newIndex, 1);
-    console.log("state:", state);
+    return state.remainingVids.splice(newIndex, 1)[0];
 }
 
 function playPause(event) {
-    // let currentVideo = event.target;
-    let currentVideo = document.getElementById(state.currentVideoId);
+    let currentVideo = videoElements[state.currentIndex];
     if ((currentVideo.currentTime <= 0 || currentVideo.paused || currentVideo.ended) && currentVideo.readyState > 2) {
         console.log("playing");
         pausedText.classList.add('hidden');
